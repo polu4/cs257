1ddb66a2aa2351134e76646799dd6fd029cd4be1
Thomas Lund <thomaslund5897@gmail.com>
2025-04-17 20:20:00 -0500

    Removed Pandas, data printing is now ugly

--- individual/cli.py
+++ individual/cli.py
@@ -4,35 +4,60 @@ Thomas Lund
 16-Apr-2025
 
 NAME: cli.py - command-line interface
-SYNOPSIS: cli.py {filepath to csv} {max number of players: int}
-DESCRIPTOPM: Prints a list of games with the inputted number of max players, sorted
-by average user rating
+SYNOPSIS: cli.py {filepath to csv} {collumn to sort by: int}
+DESCRIPTOPM: Prints the array sorted by the given collumn
 '''
 import argparse as arg
-import pandas as pd
-
+import csv
 
 def getParse():
-    parser = arg.ArgumentParser(description="Lookup the games with a number of players")
+    parser = arg.ArgumentParser(description="Sort the list of games by a given data header")
     parser.add_argument('dataLocation',metavar="file path",help="Where the data is as a csv file")
-    parser.add_argument('numPlayers',metavar="Players number",help="How many players to search for")
+    parser.add_argument('colNum',metavar="Collumn number",help="which collumn to sort by, as an int")
     args = parser.parse_args()
     return args
 
 def grabDat(fileLoc):
-    dat = pd.read_csv(fileLoc)
-    return dat.sort_values('average')[::-1] #sort by collumn 'average' then reverse for descending order
+    out = []
+    headers = []
+    fileLoc = './data.csv'
+    with open(fileLoc, newline='',encoding='utf8') as data:
+        reader = csv.reader(data)
+        isFirst = 1
+        isSecond = 0
+        for row in reader:
+            if isSecond:
+                for i in row:
+                    out.append([i])
+                isSecond=0
+            if isFirst:
+                for i in row:
+                    headers.append(i)
+                isFirst = 0
+                isSecond = 1
+            else:
+                for ind,i in enumerate(row):
+                    out[ind].append(i)
+    return out
 
-def print_full(x):
-    pd.set_option('display.max_columns', 50)
-    print(x)
-    pd.reset_option('display.max_rows')
+def sortBy(dat,collumn):
+    dat[0],dat[collumn] = dat[collumn],dat[0]
+    try:
+        dat[0] = [str(i) for i in dat[0]]
+    except:
+        pass
+    combined = list(zip(*dat))
+    combined.sort()
+    combined = list(zip(*combined))
+    combined[0],combined[collumn] = combined[collumn],combined[0]
+    print(combined[collumn])
+    return combined
 
 def main():
     args = getParse()
     dat = grabDat(args.dataLocation)
-    print(args.numPlayers)
-    print(dat[dat["maxplayers"]==int(args.numPlayers)][["name","average","minplayers","maxplayers","playingtime"]]) # print certain collumns of the dataframe for rows with max players=x
-
+    dat = sortBy(dat,7)
+    print(args.colNum)
+    print(dat)
 
 main()
