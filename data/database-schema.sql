612795b3155db9e58548cb3ce1ccd2f06819842d
Thomas Lund <thomaslund5897@gmail.com>
2025-04-29 20:43:58 -0500

    added database-schema

---
+++ data/database-schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE games(
+id int NOT NULL,
+name text,
+designer text,
+publisher text,
+artist text,
+yearPublished int,
+minPlayers int,
+maxPlayers int,
+playTime int,
+minPlayTime int,
+maxPlayTime int,
+age int,
+rating int);
